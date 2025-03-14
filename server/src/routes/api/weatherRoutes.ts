ffa417190bd07e5d45a497a8004570fdf812a7e8
dalya kablawi <dalyakablawi@gmail.com>
2025-03-13 20:31:24 -0400

    1/2

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,43 @@
-import { Router, type Request, type Response } from 'express';
+import { Router, type Request, type Response } from "express";
+import HistoryService from "../../service/historyService.js";
+import WeatherService from "../../service/weatherService.js";
 const router = Router();
 
 // import HistoryService from '../../service/historyService.js';
 // import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req: Request, res: Response) => {
-  // TODO: GET weather data from city name
-  // TODO: save city to search history
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    const weatherData = await WeatherService.getWeather(cityName);
+
+    await HistoryService.addCityToHistory(cityName);
+
+    res.status(200).json(weatherData);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error retrieving weather data",
+      error: error.message,
+    });
+  }
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get("/history", async (req: Request, res: Response) => {
+  try {
+    const history = await HistoryService.getSearchHistory();
+    res.status(200).json(history);
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "Error retrieving search history",
+        error: error.message,
+      });
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete("/history/:id", async (req: Request, res: Response) => {});
 
 export default router;
